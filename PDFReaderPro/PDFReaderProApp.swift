fe3a856f8c062cb84151e193b02f41c7fecb9d0d
Bruna Baudel <brubaudel@gmail.com>
2022-12-02 15:03:59 +0100

    Initial Commit

---
+++ PDFReaderPro/PDFReaderProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFReaderProApp.swift
+//  PDFReaderPro
+//
+//  Created by Bruna Baudel on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PDFReaderProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
